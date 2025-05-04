55cfeb2178142ce4ff6d099ecc5d158e65c899c1
aleksandr <a.d.beznutrov@urfu.me>
2025-05-04 18:31:25 +0500

    project created

---
+++ src/test/java/com/weykko/librarysystem/LibrarySystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.weykko.librarysystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibrarySystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
