1bc26543521924d777f604459c106e3b9f4f01a5
aleksandr <a.d.beznutrov@urfu.me>
2025-05-18 23:07:29 +0500

    😋

--- src/main/java/com/weykko/librarysystem/controller/AdminLoanController.java
+++ src/main/java/com/weykko/librarysystem/controller/AdminLoanController.java
@@ -4,6 +4,7 @@ import com.weykko.librarysystem.dto.loan.BorrowBookRequest;
 import com.weykko.librarysystem.dto.loan.LoanResponse;
 import com.weykko.librarysystem.dto.loan.ReturnBookRequest;
 import com.weykko.librarysystem.dto.loan.ReturnBookResponse;
+import com.weykko.librarysystem.service.LoanService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -15,29 +16,31 @@ import java.util.List;
 @RequestMapping("/api/admin/loans")
 public class AdminLoanController {
 
+    private final LoanService loanService;
+
     @GetMapping
     public List<LoanResponse> getAllLoans() {
-
+        return loanService.getAllLoans();
     }
 
     @GetMapping("/{id}")
     public LoanResponse getLoanById(@PathVariable Long id) {
-
+        return loanService.getLoanById(id);
     }
 
     @GetMapping("/overdue")
     public List<LoanResponse> getOverdueLoans() {
-
+        return loanService.getOverdueLoans();
     }
 
     @PostMapping("/borrow")
     @ResponseStatus(HttpStatus.CREATED)
     public LoanResponse borrowBook(@RequestBody BorrowBookRequest request) {
-
+        return loanService.borrowBook(request);
     }
 
     @PostMapping("/{id}/return")
-    public ReturnBookResponse returnBook(@PathVariable Long id) {
-
+    public void returnBook(@PathVariable Long id) {
+        loanService.returnBook(id);
     }
 }
