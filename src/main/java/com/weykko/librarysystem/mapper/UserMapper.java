45e64cceb2a989751950a7577bba9b6523bb4a13
aleksandr <a.d.beznutrov@urfu.me>
2025-05-28 23:58:06 +0500

    added mapper

---
+++ src/main/java/com/weykko/librarysystem/mapper/UserMapper.java
@@ -0,0 +1,13 @@
+package com.weykko.librarysystem.mapper;
+
+import com.weykko.librarysystem.dto.auth.RegisterRequest;
+import com.weykko.librarysystem.entity.UserEntity;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserMapper {
+
+    public UserEntity toEntity(RegisterRequest request) {
+
+    }
+}
