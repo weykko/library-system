451f9f102ffbffdd4048baec31168633321d290f
aleksandr <a.d.beznutrov@urfu.me>
2025-05-30 15:39:38 +0500

    created new annotation

--- src/main/java/com/weykko/librarysystem/exception/TimeoutException.java
+++ src/main/java/com/weykko/librarysystem/exception/TimeoutException.java
@@ -2,6 +2,6 @@ package com.weykko.librarysystem.exception;
 
 public class TimeoutException extends RuntimeException {
     public TimeoutException(Long interval) {
-        super("Method cannot be called more than once every " + interval + " seconds");
+        super("Method can only be called once every " + interval + " seconds");
     }
 }
